033ed11c4d0996e826a2b2bdec7d7db35809f0e1
M-Sanli <mustafasanli1995@gmail.com>
2021-05-05 16:18:47 +0200

    Final version of the code file

--- soup/soup.py
+++ soup/soup.py
@@ -1,7 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
+url = "https://waterstones.com/books/bestsellers"
+response = requests.get(url)
 
-print()
+html_icerigi = response.content
 
+soup = BeautifulSoup(html_icerigi, "html.parser")
 
+d = pd.DataFrame({'Name' : [], 'Author' : [], 'Price' : []})
+
+for i in range(len(soup.find_all("div", {"class" : "title-wrap"}))):
+    try:
+        name = soup.find_all("div", {"class" : "title-wrap"})[i].text.replace("\n", "")
+    except:
+        name = ""
+
+    try:
+        author = soup.find_all("span", {"class" : "author"})[i].text.replace("\n", "")
+    except:
+        author = ""
+
+    try:
+        price = soup.find_all("span", {"class" : "price"})[i].text.replace("\n", "")
+    except:
+        price = ""
+
+    dataframe = {"Name" : name, "Author" : author, "Price" : price}
+    d = d.append(dataframe, ignore_index=True)
+print(d)
+
+d.to_csv("data_bs.csv", mode = "a", header = True, index = False)
